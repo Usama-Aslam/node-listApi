cef5d258e466b77b3abc3bae6bc9a3470e4d35bc
UsamaAslam <uaslam345@gmail.com>
2019-03-04 20:14:17 +0500

    POST /list is configures

--- server/Server.js
+++ server/Server.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const bodyParse = require("body-parser");
+const _ = require("lodash");
 
 const { mongoose } = require("./db/mongoose");
 const { List } = require("./models/List");
 const { User } = require("./models/User");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParse.json());
+
+app.post("/lists", (req, res) => {
+  var body = _.pick(req.body, ["text", "category"]);
+
+  const newList = new List(body);
+  newList
+    .save()
+    .then(list => {
+      console.log("Insert Successful", list);
+      res.status(200).send(list);
+    })
+    .catch(e => res.status(400).send(e));
+});
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
