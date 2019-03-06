64b521bb73feb2c1c2cd81e4d46c632cc62fcb4d
UsamaAslam <uaslam345@gmail.com>
2019-03-06 21:53:35 +0500

    GET /list/:id testing is done

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,11 +1,15 @@
 const request = require("supertest");
 const expect = require("expect");
+const { ObjectID } = require("mongodb");
 
 const { app } = require("../Server");
 const { List } = require("../models/List");
 // const { User } = require("../models/User");
 
-const lists = [{ text: "first test list" }, { text: "second test list" }];
+const lists = [
+  { text: "first test list", _id: new ObjectID() },
+  { text: "second test list", _id: new ObjectID() }
+];
 
 beforeEach(done => {
   List.remove({})
@@ -64,3 +68,33 @@ describe("GET /lists", () => {
   });
 });
 
+describe("GET /lists/:id", () => {
+  it("should get particular doc from lists", done => {
+    var hexId = lists[0]._id.toHexString();
+
+    request(app)
+      .get(`/lists/${hexId}`)
+      .expect(200)
+      .expect(res => {
+        // console.log(res.body.list);
+        expect(res.body.list.text).toBe(lists[0].text);
+      })
+      .end(done);
+  });
+
+  it("should return 400 for invalid id", done => {
+    request(app)
+      .get("/lists/12354")
+      .expect(400)
+      .end(done);
+  });
+
+  it("should return 404 if no list found", done => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .get(`/lists/${hexId}`)
+      .expect(404)
+      .expect(res => expect(res.body.error).toEqual("list not found"))
+      .end(done);
+  });
+});
